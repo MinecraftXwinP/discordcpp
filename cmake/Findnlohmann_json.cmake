0ad2bbf313ebed3ab28e6342724712c77e2f4fd2
minecraftxwinp <tony84727@gmail.com>
2018-06-25 13:31:05 +0800

    fix casing in CMakeLists.txt. add hint in Findnlohmann_json module.

--- cmake/Findnlohmann_json.cmake
+++ cmake/Findnlohmann_json.cmake
@@ -1,5 +1,15 @@
+include(FindPackageHandleStandardArgs)
+
 find_path(
     nlohmann_json_INCLUDE_DIRS
+    HINTS
+        ENV NLOHMANN_JSON_DIR
     NAMES
     nlohmann/json.hpp
+)
+
+find_package_handle_standard_args(
+    nlohmann_json
+    DEFAULT_MSG
+    nlohmann_json_INCLUDE_DIRS
 )
