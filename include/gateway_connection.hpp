43c9ad21d80431bbafb16e907888589770e1bb5a
minecraftxwinp <tony84727@gmail.com>
2018-07-04 13:30:47 +0800

    added gateway_connection. WIP.

---
+++ include/gateway_connection.hpp
@@ -0,0 +1,21 @@
+#ifndef _H_GATEWAY_CONNECTION_
+#define _H_GATEWAY_CONNECTION_
+#include <boost/beast/websocket.hpp>
+#include "asio_port.hpp"
+#include "ssl_socket_factory.hpp"
+#include "payload.hpp"
+#include "discord_api.hpp"
+
+namespace discordcpp {
+    class gateway_connection {
+        public:
+            gateway_connection(io_context& io);
+            void send(payload p);
+        private:
+            void on_handshake(boost::system::error_code ec);
+            void on_ssl_handshake(boost::system::error_code ec);
+            boost::beast::websocket::stream<ssl_socket> socket;
+            io_context& io;
+    };
+};
+#endif
---
+++ src/gateway_connection.cpp
@@ -0,0 +1,7 @@
+#include "gateway_connection.hpp"
+
+namespace discordcpp {
+    gateway_connection::gateway_connection(io_context& io): io(io) {
+
+    }
+}
